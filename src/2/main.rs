8cf30cf13074311579a4bd9e1d166c81a6b99497
ytyaru <ytyaru@outlook.jp>
2019-06-25 21:58:18 +0900

    vecを変更できない

--- src/2/main.rs
+++ src/2/main.rs
@@ -30,10 +30,22 @@ fn analize_command(s: &str, employees: &mut HashMap<String, Vec<String>>) -> boo
 //        "add" => add_employee(commands[1..]),
 //        "add" if 3 == commands.len() => add_employee(commands[1], commands[2]),
         "add" if 3 == commands.len() => {
+            let v = employees.entry(String::from(commands[1])).or_insert(Vec::new());
+//            let _: () = v;
+//            (*v).push(String::from(commands[1]));
+            v.push(String::from(commands[2]));
+            println!("{:?}", v);
+//            employees[commands[1]].push(String::from(commands[2])); // error[E0596]: cannot borrow immutable indexed content as mutable
+//            employees[String::from(commands[1])].push(String::from(commands[2])); // error[E0308]: mismatched types
+//            employees.get(commands[1]).push(String::from(commands[2])); // error[E0596]: cannot borrow immutable indexed content as mutable
+            true
+
+/*
 //            let mut v = vec![String::from(commands[2])];
             let mut v = Vec::new();
             let emps = employees.entry(String::from(commands[1])).or_insert(v);
 //            let emps = employees.entry(String::from(commands[1])).or_insert(Vec::new());
+//            emps.push(String::from(commands[2]));
             emps.push(String::from(commands[2]));
 //            let mut v = Vec::new();
 //            v.push(String::from(commands[2]));
@@ -43,6 +55,7 @@ fn analize_command(s: &str, employees: &mut HashMap<String, Vec<String>>) -> boo
 //            println!("{:?}", *employees);
             println!("{:?}", *emps);
             true
+            */
         },
         "list" => {
             println!("{:?}", *employees);
